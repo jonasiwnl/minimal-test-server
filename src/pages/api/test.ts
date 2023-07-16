a841b5f08a4ebee4f07a973467786d99c2de3f99
Jonas <jonasg@umich.edu>
2023-07-16 18:48:58 -0400

    add space

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -5,7 +5,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
  */
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<{ info: string}>
+  res: NextApiResponse<{ info: string }>
 ) {
   const info = `\n${Date.now()} ${req.method}\nHeaders: ${JSON.stringify(req.headers)} \nBody: ${JSON.stringify(req.body)}\n`
 
