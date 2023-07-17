2c234c4d1e24eaa47b367809e804529cc4068955
Jonas <jonasg@umich.edu>
2023-07-16 20:36:53 -0400

    add cors & better formatting

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -7,7 +7,30 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<{ info: string }>
 ) {
-  const info = `\n${Date.now()} ${req.method}\nHeaders: ${JSON.stringify(req.headers)} \nBody: ${JSON.stringify(req.body)}\n`
+  // CORS stuff
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+
+  let headers = "Headers:\n"
+  for (const [key, value] of Object.entries(req.headers)) {
+    headers += `${key}: ${value}\n`
+  }
+
+  let body = "Body:\n"
+  for (const [key, value] of Object.entries(req.body)) {
+    body += `${key}: ${value}\n`
+  }
+
+  const info = `\n${new Date(Date.now()).toLocaleString("en-US")} ${req.method}\n${headers} \n${body}\n`
 
   console.log(info)
   res.status(200).json({ info })
